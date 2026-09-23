305b54443f16ddaed81bfd9f9990c0a08544a88e
Kai <56639630+kkauper@users.noreply.github.com>
2026-09-23 13:20:28 +0200

    feat: add favicon and web app manifest for improved branding and PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,14 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Scout',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+      meta: [{ name: 'apple-mobile-web-app-title', content: 'Scout' }],
       script: [
         {
           innerHTML: "(function(){try{var m=localStorage.getItem('vueuse-color-scheme')||'auto';var d=m==='dark'||(m==='auto'&&matchMedia('(prefers-color-scheme: dark)').matches);document.documentElement.classList.toggle('dark',d)}catch(e){}})()",
